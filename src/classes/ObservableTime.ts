308461de2d7c8cf7451a98ec65c81e2e7398ad01
IagorSps <iagor.sps+github@gmail.com>
2026-04-14 16:56:58 -0300

    feat(Time): :art: Create class with pattern sub/pub to time entity

---
+++ src/classes/ObservableTime.ts
@@ -0,0 +1,33 @@
+import Time from "./Time";
+
+type Observable<T> = (arg: T) => void
+
+// TODO test
+export default class ObservableTime extends Time {
+    private minutesSubs = new Set<Observable<string>>();
+    private secondsSubs = new Set<Observable<string>>();
+
+    private notifyMinutes() {
+        this.minutesSubs.forEach(minutesSub => minutesSub(this.getMinutes()))
+    }
+
+    private notifySeconds() {
+        this.secondsSubs.forEach(secondsSub => secondsSub(this.getSeconds()))
+    }
+
+    public addMinutesSub(minutesSub: Observable<string>) {
+        this.minutesSubs.add(minutesSub);
+    }
+
+    public addSecondsSub(secondsSub: Observable<string>) {
+        this.secondsSubs.add(secondsSub);
+    }
+
+    // TODO optimize to notify only observables needed
+    override addTime(timeToAdd: number): void {
+        super.addTime(timeToAdd);
+
+        this.notifyMinutes();
+        this.notifySeconds();
+    }
+}
--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,1 +1,2 @@
 export { default as Time } from './Time';
+export { default as ObservableTime } from './ObservableTime';
