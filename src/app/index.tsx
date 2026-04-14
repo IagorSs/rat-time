a70bf774ff63b3d8ae8f9d092b7e5b477db27d0f
IagorSps <iagor.sps+github@gmail.com>
2026-04-14 19:58:49 -0300

    feat: :sparkles: First version of app principal screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,69 @@
+import { FilledButton } from "@/components/buttons";
+import { useLoadedFonts, useTimer } from "@/hooks";
 import { Text, View } from "react-native";
+import TimeDisplay from "./timeDisplay";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen. IAGOR</Text>
-    </View>
-  );
+    const fontsLoaded = useLoadedFonts();
+
+    const {
+        minutes,
+        seconds,
+        isTimerRunning,
+        addMinute,
+        subMinute,
+        addSecond,
+        subSecond,
+        startTimer,
+    } = useTimer();
+
+    return !fontsLoaded
+        ? (
+            <Text>
+                LOADING FONTS
+            </Text>
+        ) : (
+            <View
+                style={{
+                    flex: 1,
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <View
+                    style= {{
+                        display: "flex",
+                        flexDirection: "row"
+                    }}
+                >
+                    <TimeDisplay
+                        value={minutes}
+                        onPlusClick={addMinute}
+                        onMinusClick={subMinute}
+                        disabled={isTimerRunning}
+                    />
+
+                    <TimeDisplay
+                        value={seconds}
+                        onPlusClick={addSecond}
+                        onMinusClick={subSecond}
+                        disabled={isTimerRunning}
+                    />
+                </View>
+
+                <FilledButton
+                    value="START"
+                    onClick={startTimer}
+                    disabled={isTimerRunning}
+                    styleOverrides={{
+                        borderRadius: 6,
+                        paddingHorizontal: 16,
+                        paddingVertical: 8,
+                    }}
+                    textStyleOverrides={{
+                        fontSize: 40
+                    }}
+                />
+            </View>
+        );
 }
