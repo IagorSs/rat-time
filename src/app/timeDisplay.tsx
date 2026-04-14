dc689b55c15403ed600e31a7cba138b2f1a975a7
IagorSps <iagor.sps+github@gmail.com>
2026-04-14 19:58:21 -0300

    feat(Time): :sparkles: Create component to display time on react using the hook

---
+++ src/app/timeDisplay.tsx
@@ -0,0 +1,59 @@
+import { FilledButton } from "@/components/buttons";
+import { Title } from "@/components/texts";
+import { ColorsConstants } from "@/constants";
+import { View } from "react-native";
+
+type TimeDisplayProps = {
+    value: string;
+    onPlusClick: () => void;
+    onMinusClick: () => void;
+    disabled?: boolean;
+}
+
+export default ({
+    value, onPlusClick, onMinusClick, disabled
+}: TimeDisplayProps) => (
+    <View
+        style={{
+            marginHorizontal: 2,
+            marginVertical: 8
+        }}
+    >
+        <FilledButton
+            value="+"
+            onClick={onPlusClick}
+            disabled={disabled}
+            styleOverrides={{
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+            }}
+            textStyleOverrides={{
+                fontSize: 60
+            }}
+        />
+
+        <Title
+            value={value}
+            styleOverrides={{
+                borderLeftWidth: 2,
+                borderRightWidth: 2,
+                borderColor: disabled ? ColorsConstants.red.light : ColorsConstants.red.normal,
+                paddingHorizontal: 8,
+                fontSize: 120
+            }}
+        />
+
+        <FilledButton
+            value="-"
+            onClick={onMinusClick}
+            disabled={disabled}
+            styleOverrides={{
+                borderBottomLeftRadius: 8,
+                borderBottomRightRadius: 8,
+            }}
+            textStyleOverrides={{
+                fontSize: 60
+            }}
+        />
+    </View>
+)
