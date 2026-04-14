5125fb88fa829fa9f49388dd67ff1f8178b5361a
IagorSps <iagor.sps+github@gmail.com>
2026-04-14 19:50:11 -0300

    feat: :sparkles: Create filled button component

---
+++ src/components/buttons/filledButton.tsx
@@ -0,0 +1,44 @@
+import { ColorsConstants, TextsConstants } from "@/constants";
+import { Text, TouchableOpacity } from "react-native"
+
+type FilledButtonProps = {
+    value: string;
+    disabled?: boolean;
+    onClick?: () => void;
+    styleOverrides?: {
+        borderTopRightRadius?: number;
+        borderTopLeftRadius?: number;
+        borderBottomLeftRadius?: number;
+        borderBottomRightRadius?: number;
+        borderRadius?: number;
+        paddingHorizontal?: number;
+        paddingVertical?: number;
+    };
+    textStyleOverrides?: {
+        fontSize?: number;
+    }
+}
+
+export default ({
+    value, disabled, onClick, styleOverrides: commonStyleOverrides, textStyleOverrides
+}: FilledButtonProps) => (
+    // TODO custom click animation
+    <TouchableOpacity
+        onPress={onClick}
+        disabled={disabled}
+        style={commonStyleOverrides}
+    >
+        <Text
+            style={{
+                ...TextsConstants.title,
+                backgroundColor: disabled ? ColorsConstants.red.light : ColorsConstants.red.normal,
+                color: ColorsConstants.white.normal,
+                textAlign: "center",
+                ...commonStyleOverrides,
+                ...textStyleOverrides,
+            }}
+        >
+            {value}
+        </Text>
+    </TouchableOpacity>
+);
